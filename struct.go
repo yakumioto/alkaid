76105089aac7f2b574d0dfbdeabed3ac2be923ba
yakumioto <yaku.mioto@gmail.com>
2019-12-09 08:35:38 +0800

    feat: update syschannel flag

--- struct.go
+++ struct.go
@@ -1,11 +1,28 @@
 package main
 
+type SystemBlock struct {
+	Data struct {
+		Data []struct {
+			Payload struct {
+				Data struct {
+					Config     *SystemConfig          `json:"config"`
+					LastUpdate map[string]interface{} `json:"last_update"`
+				} `json:"data"`
+				Header map[string]interface{} `json:"header"`
+			} `json:"payload"`
+			Signature string `json:"signature"`
+		} `json:"data"`
+	} `json:"data"`
+	Header   map[string]interface{} `json:"header"`
+	Metadata map[string]interface{} `json:"metadata"`
+}
+
 type Block struct {
 	Data struct {
 		Data []struct {
 			Payload struct {
 				Data struct {
-					Config     Config                 `json:"config"`
+					Config     *Config                `json:"config"`
 					LastUpdate map[string]interface{} `json:"last_update"`
 				} `json:"data"`
 				Header map[string]interface{} `json:"header"`
@@ -27,6 +44,36 @@ type Config struct {
 				Values    map[string]interface{} `json:"values"`
 				Version   string                 `json:"version"`
 			} `json:"Application"`
+			Orderer struct {
+				Groups    map[string]interface{} `json:"groups"`
+				ModPolicy string                 `json:"mod_policy"`
+				Policies  map[string]interface{} `json:"policies"`
+				Values    map[string]interface{} `json:"values"`
+				Version   string                 `json:"version"`
+			} `json:"Orderer"`
+		} `json:"groups"`
+		ModPolicy string                 `json:"mod_policy"`
+		Policies  map[string]interface{} `json:"policies"`
+		Values    map[string]interface{} `json:"values"`
+		Version   string                 `json:"version"`
+	} `json:"channel_group"`
+	Sequence string `json:"sequence"`
+}
+
+type SystemConfig struct {
+	ChannelGroup struct {
+		Groups struct {
+			Consortiums struct {
+				Groups struct {
+					SampleConsortium struct {
+						Groups    map[string]interface{} `json:"groups"`
+						ModPolicy string                 `json:"mod_policy"`
+						Policies  map[string]interface{} `json:"policies"`
+						Values    map[string]interface{} `json:"values"`
+						Version   string                 `json:"version"`
+					} `json:"SampleConsortium"`
+				} `json:"groups"`
+			} `json:"Consortiums"`
 			Orderer struct {
 				Groups    map[string]interface{} `json:"groups"`
 				ModPolicy string                 `json:"mod_policy"`
